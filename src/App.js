0533b22d746efac08ad3145cab0c5027aa147789
Hyeyeon Seo <skojphy@gmail.com>
2022-01-08 18:02:03 +0900

    fix: Modify parameters of useState function
    To improve performance

--- src/App.js
+++ src/App.js
@@ -3,54 +3,35 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+const createBulkTodos = () =>
+  new Array(2500).fill().map((el, i) => ({
+    id: i + 1,
+    text: `Todo ${i + 1}`,
+    checked: false,
+  }));
+
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'React',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: 'TypeScript',
-      check: true,
-    },
-    {
-      id: 3,
-      text: 'JavaScript Review',
-      check: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos);
+  const nextId = useRef(todos.length + 1);
 
-  const nextId = useRef(4);
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
-
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo)));
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) => todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo)));
+  }, []);
 
   return (
     <TodoTemplate>
